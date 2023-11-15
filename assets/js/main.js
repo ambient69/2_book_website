f2c342c926b09dbaf38dedf455df168e61aafb0d
Nikita Kozlenko <kozlenko_n@knu.ua>
2023-11-15 16:16:39 +0200

    section Home & Shadow Header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,67 +1,77 @@
 /*=============== SEARCH ===============*/
-const searchButton = document.getElementById('search-button')
-const searchClose = document.getElementById('search-close')
-const searchContent = document.getElementById('search-content')
-
+const searchButton = document.getElementById("search-button");
+const searchClose = document.getElementById("search-close");
+const searchContent = document.getElementById("search-content");
 
 if (searchButton) {
-    searchButton.addEventListener('click',()=>{
-        searchContent.classList.add('show-search')
-    })
-    
+  searchButton.addEventListener("click", () => {
+    searchContent.classList.add("show-search");
+  });
 }
 
 if (searchClose) {
-    searchClose.addEventListener('click',()=>{
-        searchContent.classList.remove('show-search')
-    })
-    
+  searchClose.addEventListener("click", () => {
+    searchContent.classList.remove("show-search");
+  });
 }
 
 /*=============== LOGIN ===============*/
-const loginButton = document.getElementById('login-button')
-const loginClose = document.getElementById('login-close')
-const loginContent = document.getElementById('login-content')
-
+const loginButton = document.getElementById("login-button");
+const loginClose = document.getElementById("login-close");
+const loginContent = document.getElementById("login-content");
 
 if (loginButton) {
-    loginButton.addEventListener('click',()=>{
-        loginContent.classList.add('show-login')
-    })
-    
+  loginButton.addEventListener("click", () => {
+    loginContent.classList.add("show-login");
+  });
 }
 
 if (loginClose) {
-    loginClose.addEventListener('click',()=>{
-        loginContent.classList.remove('show-login')
-    })
-    
+  loginClose.addEventListener("click", () => {
+    loginContent.classList.remove("show-login");
+  });
 }
 
-
-
 /*=============== ADD SHADOW HEADER ===============*/
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
 
+window.addEventListener('scroll',shadowHeader);
 
 /*=============== HOME SWIPER ===============*/
 
+const swiper = new Swiper(".home__swiper", {
+  // Optional parameters
 
+  loop: true,
+  spaceBetween: -24,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    1220: {
+      spaceBetween: -32,
+    },
+  },
+});
 /*=============== FEATURED SWIPER ===============*/
-
 
 /*=============== NEW SWIPER ===============*/
 
-
 /*=============== TESTIMONIAL SWIPER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
